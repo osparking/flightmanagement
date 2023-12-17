e7e8d4b1c267e5c0f7db7c301cf6a731c6dd06fa
osparking <jbpark03@gmail.com>
2023-12-17 21:46:28 +0900

    mvn 명령으로 생성, gitignore 복사

---
+++ src/main/java/space/bum/flights/App.java
@@ -0,0 +1,13 @@
+package space.bum.flights;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
