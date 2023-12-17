e137d783df3575fac8b00d30298f400c6db92cae
osparking <jbpark03@gmail.com>
2023-12-17 22:25:07 +0900

    신 포맷 적용- 들여쓰기 단위: 공백 2개.

--- src/main/java/space/bum/flights/App.java
+++ src/main/java/space/bum/flights/App.java
@@ -4,10 +4,8 @@ package space.bum.flights;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
